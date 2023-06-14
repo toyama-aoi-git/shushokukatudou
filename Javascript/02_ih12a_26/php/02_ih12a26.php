6fc344a387273192b5cc43735d158648ddfb1710
toyamaAoi <yuanshankui652@gmail.com>
2023-06-14 21:36:09 +0900

    諸事情により削除

--- Javascript/02_ih12a_26/php/02_ih12a26.php
+++
@@ -1,7 +0,0 @@
-<?php
-    header('Access-Control-Allow-Origin:*');
-    header("Content-type: application/xml; charset=UTF-8");
-    $url = $_GET['url'];
-    $xml = file_get_contents($url);
-    echo $xml;
-?>
